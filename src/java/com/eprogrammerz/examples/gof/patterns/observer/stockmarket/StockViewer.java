f3f4c8734b70a4e9cf7208563e59eca3e884827d
Yogen Rai <yogen.rai.992@gmail.com>
2018-10-28 15:35:58 -0700

    Updated diagrams for observer pattern

--- src/java/com/eprogrammerz/examples/gof/patterns/observer/stockmarket/StockViewer.java
+++ src/java/com/eprogrammerz/examples/gof/patterns/observer/stockmarket/StockViewer.java
@@ -9,7 +9,6 @@ public class StockViewer implements StockBroker {
 
     public void update(Map<String, Double> stocks) {
         System.out.println("StockViewer: stockList is changed:");
-
         stocks.forEach((symbol, value) -> System.out.println(symbol + " - $" + value));
     }
 }
